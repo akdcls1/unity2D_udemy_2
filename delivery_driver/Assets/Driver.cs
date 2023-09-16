08b552b837af0b9ce9de545b73f66b9714901af9
akdcls1 <56528690+akdcls1@users.noreply.github.com>
2023-09-16 22:23:22 +0900

    section_2_18
    section_2_18_complete

--- delivery_driver/Assets/Driver.cs
+++ delivery_driver/Assets/Driver.cs
@@ -4,19 +4,22 @@ using UnityEngine;
 
 public class Driver : MonoBehaviour
 {
+    // unity 상에서 제어가능 추가 -> SerializeField
     [SerializeField] float steerSpeed = 0.1f;
 
     [SerializeField] float moveSpeed = 0.01f;
 
-    void Start()
+    void Start() // 시작 시에만 적용 시 스타트에 소스 추가
     {
         
     }
 
-    void Update()
+    void Update() // 실시간 적용 시 업데이트에 소스 추가
     {
+        //Input.getAxis에 Input Manager Axes 값 넣으면 해당 버튼 클릭시 제어가능
         float steerAmount = Input.GetAxis("Horizontal") * steerSpeed * Time.deltaTime;
         float moveAmount = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
+        // transform 으로 돌거나(Rotate) 속도(Translate)변경 가능
         transform.Rotate(0, 0, -steerAmount);
         transform.Translate(0, moveAmount, 0);
     }
