21321090dfafcba5c7328d87236868cf6e1545a1
akdcls1 <56528690+akdcls1@users.noreply.github.com>
2023-09-27 01:51:16 +0900

    section_2_25
    section_2_25_complete

--- delivery_driver/Assets/Delivery.cs
+++ delivery_driver/Assets/Delivery.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Delivery : MonoBehaviour
 {
     bool hasPackage;
+    [SerializeField] float destroyDelay = 0.5f;
     void OnCollisionEnter2D(Collision2D other) // 충돌메서드
     { 
         Debug.Log("Shit!");
@@ -12,10 +13,11 @@ public class Delivery : MonoBehaviour
 
     void OnTriggerEnter2D(Collider2D other) {
         // 패키지 픽업했을경우
-        if(other.tag == "Package")
+        if(other.tag == "Package" && !hasPackage)
         {
             Debug.Log("Package pick up!");
             hasPackage = true;
+            Destroy(other.gameObject, destroyDelay);
         }
 
         if(other.tag == "Customer" && hasPackage)
