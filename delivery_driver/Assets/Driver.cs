08a1aa6d9b513104175b36830975ee06c61ae5b9
akdcls1 <56528690+akdcls1@users.noreply.github.com>
2023-10-08 22:37:47 +0900

    section_2_complete
    section_2_end

--- delivery_driver/Assets/Driver.cs
+++ delivery_driver/Assets/Driver.cs
@@ -26,6 +26,10 @@ public class Driver : MonoBehaviour
         transform.Translate(0, moveAmount, 0);
     }
 
+    void OnCollisionEnter2D(Collision2D other) {
+        moveSpeed = slowSpeed;
+    }
+
     void OnTriggerEnter2D(Collider2D other) 
     {
         if (other.tag == "Boost")
